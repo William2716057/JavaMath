2cbcb0e6043694643351d7da98e19a7c994b0d50
William <willspri@gmail.com>
2024-02-24 12:56:36 +1300

    committed

---
+++ JavaMath.java
@@ -0,0 +1,16 @@
+public class JavaMath {
+	public static void main(String[] args) {
+		int a = 5; //add integer here
+		int b = 12; //add integer here
+		int sum = a + b; // add two integers together
+		int difference = a - b; //show the difference between the two numbers
+		int product = a * b;
+		double quotient = (double) a / b; //cast to get double result
+		int remainder = a % b; 
+		System.out.println("Sum =: " + sum);
+		System.out.println("Difference: " + difference);
+		System.out.println("Product: " + product);
+		System.out.println("Quotient " + quotient);
+		System.out.println("Remainder " + remainder);
+	}
+}
